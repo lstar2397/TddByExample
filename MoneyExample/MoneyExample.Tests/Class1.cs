07dd09a432e56a32418dcf1ec202fd05af4085fe
Starlet <lstar2397@naver.com>
2021-05-02 23:01:19 +0900

    곱셈 예제 테스트 케이스

--- MoneyExample/MoneyExample.Tests/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MoneyExample.Tests
-{
-    public class Class1
-    {
-    }
-}
---
+++ MoneyExample/MoneyExample.Tests/TestCase.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace MoneyExample.Tests
+{
+    [TestFixture]
+    public class TestCase
+    {
+        [Test]
+        public void TestMultiplication()
+        {
+            Dollar five = new Dollar(5);
+            five.Times(2);
+            Assert.Equals(10, five.amount);
+        }
+    }
+}
